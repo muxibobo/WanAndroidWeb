a58e2c4fca06df22ce3c5343bd35e2e9fc3ffe88
木溪bobo <1341741925@qq.com>
2024-04-16 09:15:20 +0800

    上传

--- mypage/common-js/appConstData.js
+++ mypage/common-js/appConstData.js
@@ -1,5 +1,4 @@
 const base_url = 'http://localhost:8888/api';
-// const base_url ='https://5o690r1070.vicp.fun/api';//内网穿透必须的域名，使用内网穿透提供外网连接，同时在网页请求中免去了配置https证书问题
 
 const pageSize = 10;//取值为[1-40]
 
